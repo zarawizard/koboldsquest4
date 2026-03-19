216de67fdde7c803888b4c57dfd3330a53ae7588
zarawizard <zara.wizard@gmail.com>
2026-03-19 08:39:45 +0100

    Add files via upload

--- koboldsquest4-rust/src/main.rs
+++ koboldsquest4-rust/src/main.rs
@@ -1,11 +1,22 @@
+//extern crate sdl2;
+
 use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
+////use sdl2::{event::Event, keyboard::Keycode, pixels::Color, rect::Rect};
+use sdl2::image::{InitFlag, LoadTexture};
+////use sdl2_image::{InitFlag, LoadTexture};
+
+
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
+    let _image_context = sdl2::image::init(InitFlag::PNG | InitFlag::JPG)?;
+//    let _image_context = sdl2_image::init;
+
     let window = video_subsystem
-        .window("rust-sdl2 example", 800, 600)
+        .window("Demon's wizard - Kobold's Quest 4", 800, 600)
+	.position_centered()
         .opengl()
         .build()
         .map_err(|e| e.to_string())?;
@@ -13,6 +24,11 @@ pub fn main() -> Result<(), String> {
     let mut event_pump = sdl_context.event_pump()?;
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    let png = "./pix/firebrand-right-32x32.png";
+
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture(png)?;
 
     'main: loop {
         for event in event_pump.poll_iter() {
@@ -27,12 +43,14 @@ pub fn main() -> Result<(), String> {
         }
 
         // Set the background
-        canvas.set_draw_color(Color::RGB(255, 200, 0));
+        canvas.set_draw_color(Color::RGB(0, 0, 255));
         canvas.clear();
 
         // Draw a red rectangle
-        canvas.set_draw_color(Color::RGB(255, 0, 0));
-        canvas.fill_rect(Rect::new(100, 100, 600, 400))?;
+//        canvas.set_draw_color(Color::RGB(255, 0, 0));
+//        canvas.fill_rect(Rect::new(100, 100, 600, 400))?;
+
+	canvas.copy(&texture, None, None)?;
 
         // Show it on the screen
         canvas.present();
